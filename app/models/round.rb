baa41c11a58de99e7b554f3bf9b00da745885aea
asc5025 <albert.s.chun@gmail.com>
2018-10-23 20:39:06 -0400

    omar changes

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,17 @@ class Round < ApplicationRecord
   belongs_to :game
   has_many :statements
   has_many :players, through: :statements
+
+  def points_tally
+   @player = Player.find(params[:position] == true)
+   self.statements.each do |statement|
+     @player.statements.each do |guess|
+       if statement.content == @guess.content && statement.answer == @guess.answer
+         @player.points += 2
+       else
+         statement.player.points += 1
+       end
+     end
+  end
+  
 end
